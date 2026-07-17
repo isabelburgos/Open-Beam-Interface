96f7a421aee4558d5c569f4b8b3f0dbd574f2828
isabelburgos <isabelmburgos01@gmail.com>
2026-07-16 20:21:33 -0700

    docs: small cosmetic tweak

--- software/docs/source/conf.py
+++ software/docs/source/conf.py
@@ -60,12 +60,12 @@ html_static_path = ['_static']
 html_favicon = '_static/favicon.png'
 html_theme_options = {
     "light_css_variables": {
-        "color-brand-primary": "#FF4F00",
+        "color-brand-primary": "#2E5F04",
         "color-brand-content": "#2EA22A",
-        "color-brand-visited": "#1F7B1E"
+        "color-brand-visited": "#1F7B1E",
     },
     "dark_css_variables": {
-        "color-brand-primary": "#FF4F00",
+        "color-brand-primary": "#B3FF01",
         "color-brand-content": "#2EA22A",
         "color-brand-visited": "#1F7B1E",
         "color-highlight-on-target": "#0B2B0B",
@@ -84,4 +84,4 @@ def linkcode_resolve(domain, info):
     if not info['module']:
         return None
     filename = info['module'].replace('.', '/')
-    return "https://github.com/nanographs/Open-Beam-Interface/blob/update-main/software/%s.py" % filename
+    return "https://github.com/isabelburgos/Open-Beam-Interface/blob/update-main/software/%s.py" % filename
